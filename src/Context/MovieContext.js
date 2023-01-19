c7ca0dfcc8a0915090c26f48ab2b1831b06ff92e
chandu <chandrasekhardamarla24@gmail.com>
2023-01-19 12:45:15 +0000

    first commit

---
+++ src/Context/MovieContext.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MovieContext = React.createContext({
+  username: '',
+  password: '',
+  closeNavBar: false,
+  triggerCloseNavBar: () => {},
+  triggerChangeUsername: () => {},
+  triggerChangePassword: () => {},
+  triggerLogout: () => {},
+  searchInput: '',
+  triggerSearchChange: () => {},
+})
+export default MovieContext
